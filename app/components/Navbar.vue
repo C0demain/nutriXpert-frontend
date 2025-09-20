8d81cd8423a091efed9fbd593f948914928413b0
BrunoVieira003 <brunosilverio01@gmail.com>
2025-09-20 18:55:56 -0300

    feat: add navbar lateral
    A navbar só é exibida se o usuário estiver logado

---
+++ app/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <Drawer v-model:visible="visible" header="Menu" v-if="authStore.isLoggedIn">
+            <div class="flex flex-col justify-between">
+                <div class="flex flex-col">
+                    <NuxtLink to="" class="opacity-40 p-2 rounded-lg hover:text-primary hover:bg-zinc-100 dark:hover:bg-zinc-800 align-middle">Chat</NuxtLink>
+                    <NuxtLink to="/anamnese" class="p-2 rounded-lg hover:text-primary hover:bg-zinc-100 dark:hover:bg-zinc-800 align-middle">Anamnese</NuxtLink>
+                    <NuxtLink to="" class="opacity-40 p-2 rounded-lg hover:text-primary hover:bg-zinc-100 dark:hover:bg-zinc-800 align-middle">Perfil</NuxtLink>
+                </div>
+            </div>
+            <template #footer>
+                <Button outlined label="Logout" @click="handleLogout" icon="pi pi-sign-out" class="!mx-auto"/>
+            </template>
+        </Drawer>
+        <Button @click="visible = true" v-if="authStore.isLoggedIn" label="Menu" variant="text" class="mb-4" icon="pi pi-bars"/>
+</template>
+
+<script setup>
+const visible = ref(false)
+const toast = useToast()
+
+const authStore = useAuthStore()
+
+function handleLogout(){
+    authStore.logout()
+    toast.add({severity: 'info', summary: 'Você saiu'})
+    navigateTo('/login')
+}
+</script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
-const authStore = useAuthStore()
-
-function handleLogout(){
-    authStore.logout()
-    navigateTo('/login')
-}
 </script>
 
 <template>
   <div>
-    <div class="flex justify-end w-10/12">
-        <Button v-if="authStore.isLoggedIn" outlined label="Logout" @click="handleLogout"/>
-    </div>
+    <Navbar/>
     <slot /> 
   </div>
 </template>
