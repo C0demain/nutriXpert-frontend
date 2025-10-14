f33fc408bed0297c82198cd3dc8f7910306c7209
BrunoVieira003 <brunosilverio01@gmail.com>
2025-10-13 21:06:27 -0300

    feat: atualiza anamnese de acordo com o backend
    A anamnese agora reflete os atributos usados no formulario fornecido e no backend

--- app/pages/anamnese.vue
+++ app/pages/anamnese.vue
@@ -1,36 +1,84 @@
 <script setup lang="ts">
-interface UserResponse{
-    id: string,
-    name: string,
-    email: string,
-    role: string,
-    height: number | null
-    weight: number | null,
-    habits: string | null,
-    illnesses: string | null,
-}
+import { SelectButton } from 'primevue'
+import { AlcoholConsuptions, AllergyIntoleranceTypes, EvacuationFrequencies, GoalTypes, HealthConditions, Hydrations, NightAwakeningFrequencies, PhysicalActivityDurations, PhysicalActivityFrequencies, PhysicalActivityType, SleepQualities, StressLevels, SurgeryTypes, type AlcoholConsuption, type AllergyIntolerance, type Anamnese, type EvacuationFrequency, type GoalType, type HealthConditionType, type Hydration, type NightAwakeningFrequency, type PhysicalActivity, type PhysicalActivityDuration, type PhysicalActivityFrequency, type SleepQuality, type StressLevel, type Surgery } from '~/types/Anamnese'
+
+
 const toast = useToast()
 const authStore = useAuthStore()
-const {data: userInfo} = await useAPI<UserResponse>(`/user/${authStore.userId}`)
 
-const illnessOpts = ['Diabetes', 'Intolerância à lactose', 'Celíaco']
-const exerciseFrequencyOpts = ['Diariamente', '2 a 3 vezes na semana', '1 vez na semana', 'Raramente']
+const activeStep = ref<string>("1")
 
-const weight = ref(userInfo.value?.weight)
-const height = ref(userInfo.value?.height)
-const illnesses = ref(userInfo.value?.illnesses?.split(','))
-const exerciseDescription = ref(userInfo.value?.habits?.split(',')[0])
-const exerciseFrequency = ref(userInfo.value?.habits?.split(',')[1])
-const doesExercise = ref(!!userInfo.value?.habits)
+// Campos da anamnese
+const goalType = ref<GoalType>()
+const goalTypeOther = ref<string>("")
+const healthConditionType = ref<HealthConditionType>()
+const healthConditionOther = ref<string>("")
+const allergyIntoleranceType = ref<AllergyIntolerance>()
+const allergyIntoleranceOther = ref<string>("")
+const surgeryType = ref<Surgery>()
+const surgeryTypeOther = ref<string>("")
+const physicalActivityType = ref<PhysicalActivity>()
+const physicalActivityOther = ref<string>("")
+const physicalActivityFrequency = ref<PhysicalActivityFrequency>()
+const physicalActivityDuration = ref<PhysicalActivityDuration>()
+const sleepQuality = ref<SleepQuality>("regular")
+const nightAwakeningFrequency = ref<NightAwakeningFrequency>("nao")
+const evacuationFrequencyType = ref<EvacuationFrequency>()
+const stressLevel = ref<StressLevel>()
+const alcoholConsumption = ref<AlcoholConsuption>()
+const tabagism = ref<boolean>(false)
+const hydration = ref<Hydration>()
+const continuousMedication = ref<boolean>(false)
+
+// Preenche caso já exista uma anamnese associada
+const {data: oldAnamnese} = await useAPI<Anamnese>(`/user/${authStore.userId}/anamnese`)
+if(oldAnamnese.value){
+    goalType.value = oldAnamnese.value.goalType
+    goalTypeOther.value = oldAnamnese.value.goalTypeOther
+    healthConditionType.value = oldAnamnese.value.healthConditionType
+    healthConditionOther.value = oldAnamnese.value.healthConditionOther
+    allergyIntoleranceType.value = oldAnamnese.value.allergyIntoleranceType
+    allergyIntoleranceOther.value = oldAnamnese.value.allergyIntoleranceOther
+    surgeryType.value = oldAnamnese.value.surgeryType
+    surgeryTypeOther.value = oldAnamnese.value.surgeryTypeOther
+    physicalActivityType.value = oldAnamnese.value.physicalActivityType
+    physicalActivityOther.value = oldAnamnese.value.physicalActivityOther
+    physicalActivityFrequency.value = oldAnamnese.value.physicalActivityFrequency
+    physicalActivityDuration.value = oldAnamnese.value.physicalActivityDuration
+    sleepQuality.value = oldAnamnese.value.sleepQuality
+    nightAwakeningFrequency.value = oldAnamnese.value.nightAwakeningFrequency
+    evacuationFrequencyType.value = oldAnamnese.value.evacuationFrequencyType
+    stressLevel.value = oldAnamnese.value.stressLevel
+    alcoholConsumption.value = oldAnamnese.value.alcoholConsumption
+    tabagism.value = oldAnamnese.value.tabagism
+    hydration.value = oldAnamnese.value.hydration
+    continuousMedication.value = oldAnamnese.value.continuousMedication
+}
 
 async function handleSubmit(){
-    const {error} = await useAPI(`/user/anamnese/${authStore.userId}`, {
-        method: 'patch',
+    const {error} = await useAPI(`/user/${authStore.userId}/anamnese`, {
+        method: oldAnamnese.value ? 'put' : 'post',
         body: {
-            weight: weight.value,
-            height: height.value,
-            illnesses: illnesses.value?.join(','),
-            habits: doesExercise ? exerciseDescription.value + ',' + exerciseFrequency.value : null
+            goalType,
+            goalTypeOther,
+            healthConditionType,
+            healthConditionOther,
+            allergyIntoleranceType,
+            allergyIntoleranceOther,
+            surgeryType,
+            surgeryTypeOther,
+            physicalActivityType,
+            physicalActivityOther,
+            physicalActivityFrequency,
+            physicalActivityDuration,
+            sleepQuality,
+            nightAwakeningFrequency,
+            evacuationFrequencyType,
+            stressLevel,
+            alcoholConsumption,
+            tabagism,
+            hydration,
+            continuousMedication
         }
     })
 
@@ -51,39 +99,143 @@ async function handleSubmit(){
 
 <template>
     <h1 class="page-title">Anamnese</h1>
-    <form class="flex flex-col gap-6" @submit.prevent="handleSubmit">
-        <div class="flex gap-6">
-            <div class="flex flex-col gap-2">
-                <label for="weight">Peso</label>
-                <InputGroup>
-                    <InputNumber v-model="weight" id="weight"/>
-                    <InputGroupAddon>Kg</InputGroupAddon>
-                </InputGroup>
-            </div>
-            <div class="flex flex-col gap-2">
-                <label for="height">Altura</label>
-                <InputGroup>
-                    <InputNumber v-model="height" id="height"/>
-                    <InputGroupAddon>cm</InputGroupAddon>
-                </InputGroup>
-            </div>
-        </div>
-        <div class="flex flex-col gap-2">
-            <label for="illnesses">Possui alguma condição?</label>
-            <MultiSelect v-model="illnesses" :options="illnessOpts"/>
-        </div>
-        <div class="flex flex-col gap-2">
-            <label for="doesExercise">Pratica atividade física</label>
-            <ToggleSwitch v-model="doesExercise" id="doesExercise"/>
-        </div>
-        <div class="flex flex-col gap-2" v-if="doesExercise">
-            <label for="exerciseDescription">Descreva sua atividade física</label>
-            <InputText v-model="exerciseDescription" id="exerciseDescription"/>
-        </div>
-        <div class="flex flex-col gap-2" v-if="doesExercise">
-            <label for="exerciseFrequency">Com que frequência você pratica essa atividade?</label>
-            <Select :options="exerciseFrequencyOpts" id="exerciseFrequency" v-model="exerciseFrequency"/>
-        </div>
-        <Button type="submit">Enviar</Button>
+    <form @submit.prevent="handleSubmit">
+        <Stepper value="1" linear>
+            <StepList>
+                <Step value="1">Condições</Step>
+                <Step value="2">Atividade física</Step>
+                <Step value="3">Rotina diária</Step>
+            </StepList>
+            <StepPanels>
+                <StepPanel v-slot="{ activateCallback }" value="1" class="flex flex-col gap-6">
+                    <div class="flex flex-col gap-2">
+                        <label for="goalType">Qual seu principal objetivo?</label>
+                        <Select :options="GoalTypes.map(k => k)" id="goalType" v-model="goalType"/>
+                    </div>
+                    <div class="flex flex-col gap-2" v-if="false">
+                        <label for="goalTypeOther">Especifique</label>
+                        <InputText id="goalTypeOther" v-model="goalTypeOther"/>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <label for="healthCondition">Você possui ou já teve alguma condição?</label>
+                        <Select :options="HealthConditions.map(v => v)" id="healthCondition" v-model="healthConditionType"/>
+                    </div>
+                    <div class="flex flex-col gap-2" v-if="healthConditionType === 'Outro'">
+                        <label for="healthConditionOther">Especifique sua condição</label>
+                        <InputText id="healthConditionOther" v-model="healthConditionOther"/>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <label for="allergyIntoleranceType">Possui alguma alergia ou intolerância?</label>
+                        <Select :options="AllergyIntoleranceTypes.map(v => v)" id="allergyIntoleranceType" v-model="allergyIntoleranceType"/>
+                    </div>
+                    <div class="flex flex-col gap-2" v-if="allergyIntoleranceType === 'Outro'">
+                        <label for="allergyIntoleranceOther">Especifique sua alergia</label>
+                        <InputText id="allergyIntoleranceOther" v-model="allergyIntoleranceOther"/>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <label for="surgeryType">Você já realizou alguma cirurgia?</label>
+                        <Select :options="SurgeryTypes.map(v => v)" id="surgeryType" v-model="surgeryType"/>
+                    </div>
+                    <div class="flex flex-col gap-2" v-if="surgeryType === 'Outro'">
+                        <label for="surgeryTypeOther">Especifique sua cirurgia</label>
+                        <InputText id="surgeryTypeOther" v-model="surgeryTypeOther"/>
+                    </div>
+
+                    <div class="flex flex-col gap-2">
+                        <label for="continuosMedication">Toma remédio controlado?</label>
+                        <div class="flex gap-2">
+                            <label>Não</label>
+                            <ToggleSwitch id="continuosMedication" v-model="continuousMedication"/>
+                            <label>Sim</label>
+                        </div>
+                    </div>
+
+                    <Button class="w-fit self-end" label="Próximo" icon="pi pi-arrow-right" iconPos="right" @click="activateCallback('2')"/>
+                </StepPanel>
+            </StepPanels>
+            <StepPanel v-slot="{ activateCallback }" value="2" class="flex flex-col gap-6">
+                <div class="flex flex-col gap-2">
+                    <label for="physicalActivityType">Qual atividade mais te descreve?</label>
+                    <Select :options="PhysicalActivityType.map(v => v)" id="physicalActivityType" v-model="physicalActivityType"/>
+                </div>
+                <div class="flex flex-col gap-2" v-if="physicalActivityType === 'Outro'">
+                    <label for="physicalActivityOther">Especifique sua atividade</label>
+                    <InputText id="physicalActivityOther" v-model="physicalActivityOther"/>
+                </div>
+                <div class="flex flex-col gap-2" v-if="physicalActivityType !== 'Sedentário(a)' && physicalActivityType">
+                    <label>Com que frequência você pratica essa atividade?</label>
+                    <div class="space-x-2" v-for="frequency in PhysicalActivityFrequencies">
+                        <RadioButton v-model="physicalActivityFrequency" :value="frequency" :id="frequency"/>
+                        <label :for="frequency">{{ frequency }}</label>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-2" v-if="physicalActivityType !== 'Sedentário(a)' && physicalActivityType">
+                    <label>Qual a duração por dia dessa atividade?</label>
+                    <div class="space-x-2" v-for="duration in PhysicalActivityDurations">
+                        <RadioButton v-model="physicalActivityDuration" :value="duration" :id="duration"/>
+                        <label :for="duration">{{ duration }}</label>
+                    </div>
+                </div>
+                <div class="flex justify-between">
+                    <Button label="Anterior" severity="secondary" icon="pi pi-arrow-left" @click="activateCallback('1')" />
+                    <Button label="Próximo" icon="pi pi-arrow-right" iconPos="right" @click="activateCallback('3')" />
+                </div>
+            </StepPanel>
+            <StepPanel v-slot="{ activateCallback }" value="3" class="flex flex-col gap-6">
+                <div class="flex flex-col gap-2">
+                    <label for="sleepQuality">Como você descreveria sua qualidade de sono?</label>
+                    <SelectButton :options="SleepQualities.map(v => v)" id="sleepQuality" v-model="sleepQuality"/>
+                </div>
+                
+                <div class="flex flex-col gap-2">
+                    <label for="nightAwakeningFrequency">Você costuma acordar durante a noite?</label>
+                    <SelectButton :options="NightAwakeningFrequencies.map(v => v)" id="nightAwakeningFrequency" v-model="nightAwakeningFrequency"/>
+                    </div>
+                    
+                <div class="flex flex-col gap-2">
+                    <label>Quantas vezes por semana você evacua?</label>
+                    <div class="space-x-2" v-for="frequencyEvac in EvacuationFrequencies">
+                        <RadioButton v-model="evacuationFrequencyType" :value="frequencyEvac" :id="frequencyEvac"/>
+                        <label :for="frequencyEvac">{{ frequencyEvac }}</label>
+                    </div>
+                </div>
+                
+                <div class="flex flex-col gap-2">
+                    <label for="stressLevel">Como você descreveria seu nível de estresse diariamente?</label>
+                    <SelectButton :options="StressLevels.map(v => v)" id="stressLevel" v-model="stressLevel"/>
+                </div>
+                
+                <div class="flex flex-col gap-2">
+                    <label>Você costuma beber?</label>
+                    <div class="space-x-2" v-for="consumo in AlcoholConsuptions">
+                        <RadioButton v-model="alcoholConsumption" :value="consumo" :id="consumo"/>
+                        <label :for="consumo">{{ consumo }}</label>
+                    </div>
+                </div>
+
+                <div class="flex flex-col gap-2">
+                    <label for="tabagism">Você fuma?</label>
+                    <div class="flex gap-2">
+                        <label>Não</label>
+                        <ToggleSwitch id="tabagism" v-model="tabagism"/>
+                        <label>Sim</label>
+                    </div>
+                </div>
+
+                <div class="flex flex-col gap-2">
+                    <label>Quantos litros de água você bebe por dia?</label>
+                    <div class="space-x-2" v-for="hydrationFreq in Hydrations">
+                        <RadioButton v-model="hydration" :value="hydrationFreq" :id="hydrationFreq"/>
+                        <label :for="hydrationFreq">{{ hydrationFreq }}</label>
+                    </div>
+                </div>
+
+                <div class="flex justify-between mb-10">
+                    <Button label="Anterior" severity="secondary" icon="pi pi-arrow-left" @click="activateCallback('2')" />
+                    <Button class="flex pt-6" type="submit">{{!!oldAnamnese ? "Atualizar" : "Enviar"}}</Button>
+                </div>
+            </StepPanel>
+        </Stepper>
+
     </form>
-</template>
+</template>*
---
+++ app/types/Anamnese.ts
@@ -0,0 +1,64 @@
+export interface Anamnese{
+    id?: string
+    goalType: GoalType
+    goalTypeOther: string
+    healthConditionType: HealthConditionType
+    healthConditionOther: string
+    allergyIntoleranceType: AllergyIntolerance
+    allergyIntoleranceOther: string
+    surgeryType: Surgery
+    surgeryTypeOther: string
+    physicalActivityType: PhysicalActivity
+    physicalActivityOther: string
+    physicalActivityFrequency: PhysicalActivityFrequency
+    physicalActivityDuration: PhysicalActivityDuration
+    sleepQuality: SleepQuality
+    nightAwakeningFrequency: NightAwakeningFrequency
+    evacuationFrequencyType: EvacuationFrequency
+    stressLevel: StressLevel
+    alcoholConsumption: AlcoholConsuption
+    tabagism: boolean
+    hydration: Hydration
+    continuousMedication: boolean
+}
+
+export const GoalTypes = ["Emagrecimento", "Ganho de massa muscular", "Controle de diabetes", "Reeducação alimentar", "Performance física e mental", "Ganho de peso", "Perda de gordura", "Manutenção do peso"] as const
+export type GoalType = typeof GoalTypes[number]
+
+export const HealthConditions = ["Diabetes tipo 1", "Diabetes tipo 2", "Hipertensão arterial", "Dislipidemia (colesterol, triglicerídeos)", "Doença renal", "Doença hepática", "Gastrite / refluxo", "Intestino preso / diarreia", "Osteoporose", "Doença cardiovascular (infarto, insuficiência cardíaca)", "Câncer", "Depressão / Ansiedade", "Doenças autoimunes", "Outro"] as const
+export type HealthConditionType = typeof HealthConditions[number]
+
+export const AllergyIntoleranceTypes = ["Não", "Intolerância à lactose", "Sensibilidade ao glúten / doença celíaca", "Alergia alimentar", "Alergia medicamentosa", "Outro"] as const
+export type AllergyIntolerance = typeof AllergyIntoleranceTypes[number]
+
+export const SurgeryTypes = ["não", "Bariátrica", "Vesícula", "Hérnia de hiato (cirurgia do refluxo)", "Ortopédica", "Cesárea / Ginecológica", "Outro"] as const
+export type Surgery = typeof SurgeryTypes[number]
+
+export const PhysicalActivityType = ["Sedentário(a)", "Caminhada", "Musculação", "Corrida", "Crossfit", "Natação", "Outro"] as const
+export type PhysicalActivity = typeof PhysicalActivityType[number]
+
+export const PhysicalActivityFrequencies = ["1–2x por semana", "3–4x por semana", "5 ou mais vezes por semana"] as const
+export type PhysicalActivityFrequency = typeof PhysicalActivityFrequencies[number]
+
+export const PhysicalActivityDurations = ["30 min", "60 min", "90 min"] as const
+export type PhysicalActivityDuration = typeof PhysicalActivityDurations[number]
+
+export const SleepQualities = ["boa", "regular", "ruim"] as const
+export type SleepQuality = typeof SleepQualities[number]
+
+export const NightAwakeningFrequencies = ["nao", "pelo menos uma vez", "mais de uma vez"] as const
+export type NightAwakeningFrequency = typeof NightAwakeningFrequencies[number]
+
+export const EvacuationFrequencies = ["Todo dia", "5x por semana", "3x por semana", "1x por semana"] as const
+export type EvacuationFrequency = typeof EvacuationFrequencies[number]
+
+export const StressLevels = ["baixo", "moderado", "alto"] as const
+export type StressLevel = typeof StressLevels[number]
+
+export const AlcoholConsuptions = ["não consome", "Socialmente 1-2 x por semana", "Frequente 3-4 x por semana", "Uso diário"] as const
+export type AlcoholConsuption = typeof AlcoholConsuptions[number]
+
+export const Hydrations = ["Menos de 1L", "Entre 1L e 2L", "Mais de 2L"] as const
+export type Hydration = typeof Hydrations[number]
+
+
