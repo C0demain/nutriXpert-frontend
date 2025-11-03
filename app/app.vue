6c11432ea4798c3132bb98d773cea9ee1e5133ac
Bruno Vieira <brunosilverio01@gmail.com>
2025-11-03 07:23:57 -0300

    fix: arruma cookie de role de usuario

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-11/12 h-svh mx-auto">
     <Toast position="bottom-right" />
-    <NuxtLayout :name="useCookie('userRole').value === 'ADMIN' ? 'admin' : 'default'">
+    <NuxtLayout :name="useCookie('role').value === 'ADMIN' ? 'admin' : 'default'">
       <NuxtPage />
     </NuxtLayout>
   </div>
