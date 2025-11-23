d66b036a95c8d440fd5d2234fab0579dc094b2df
ryan-wakugawa <ryan.wakugawa@fatec.sp.gov.br>
2025-11-22 22:18:48 -0300

    refactor: otimiza a verificação de token e papel no middleware de autenticação

--- app/middleware/01.auth.global.ts
+++ app/middleware/01.auth.global.ts
@@ -1,9 +1,16 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    if(to.path === '/login' && !!useCookie('token').value){
-        return navigateTo(useCookie('role').value === 'ADMIN' ? '/admin/users': '/')
+    const token = useCookie('token').value
+    const role = useCookie('role').value
+    
+    if(to.path === '/login' && !!token){
+        return navigateTo(role === 'ADMIN' ? '/admin/users' : '/chat')
     }
 
-    if(!['/login', '/register', '/'].includes(to.path) && !useCookie('token').value){
-        return navigateTo({path: '/login', query: {redirectUrl: from.path} })
+    if(!['/login', '/register', '/'].includes(to.path) && !token){
+        return navigateTo({path: '/login', query: {redirectUrl: from.path}})
+    }
+    
+    if(to.path.startsWith('/admin') && role !== 'ADMIN'){
+        return navigateTo('/chat')
     }
 })
